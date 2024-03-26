cf59d92809d4b4bda842cb78ae881dd618f43a37
Vuk3110 <vuktrific@gmail.com>
2024-03-26 13:30:58 +0100

    Fixed Date validator

--- AzureWW24/Models/Validator/LeadValidator.cs
+++ AzureWW24/Models/Validator/LeadValidator.cs
@@ -13,7 +13,7 @@ namespace AzureWW24.Models.Validator
         public LeadValidator()
         {
             RuleFor(lead => lead.Source).NotEmpty();
-            RuleFor(lead => lead.Date).NotEmpty(); 
+            RuleFor(lead => lead.Date).Must(IsValidDate); 
             RuleFor(lead => lead.Size).GreaterThan(0);
             RuleFor(lead => lead.ServiceType).NotEmpty();
 
@@ -22,5 +22,12 @@ namespace AzureWW24.Models.Validator
             RuleFor(lead => lead.From).SetValidator(new LocationValidator());
             RuleFor(lead => lead.To).SetValidator(new LocationValidator());
         }
+
+        private bool IsValidDate(string date)
+        {
+            return !date.Equals(default(DateOnly));
+        }
     }
+
+    
 }
